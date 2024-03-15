562eca05f35755346822d84fb7054d7f9ed7c296
Dana Zack <danaezack@gmail.com>
2024-03-15 17:46:35 -0600

    Feat: create mock data

---
+++ src/App/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import mockData from '../mock-data';
+
+function App() {
+  console.log(mockData)
+  return (
+    <div className="App">
+      <h1>NewsWorthy</h1>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/mock-data.jsx
@@ -0,0 +1,268 @@
+const mockData = {
+  "status": "ok",
+  "totalResults": 35,
+  "articles": [
+      {
+          "source": {
+              "id": null,
+              "name": "The Athletic"
+          },
+          "author": "Randy Mueller",
+          "title": "2024 NFL free-agency tracker: From Danielle Hunter to Jamal Adams, see where top 150 players land - The Athletic",
+          "description": "NFL free agency begins at noon Monday when the tampering window opens. Track the best available players (and where they land) right here.",
+          "url": "https://theathletic.com/5318482/2024/03/14/nfl-free-agent-rankings-tracker/",
+          "urlToImage": "https://cdn.theathletic.com/app/uploads/2024/03/07191019/0308_NFL_Top150FA.png",
+          "publishedAt": "2024-03-15T00:05:52Z",
+          "content": "NFL free agency is in full swing, with the legal tampering window opening Monday and the new league year beginning Wednesday at 4 p.m. ET.\r\nThese are my updated rankings and scouting reports of the t… [+97690 chars]"
+      },
+      {
+          "source": {
+              "id": "cnn",
+              "name": "CNN"
+          },
+          "author": "Eric Levenson, Lauren del Valle, Jean Casarez, Aaron Pellish, Elise Hammond",
+          "title": "Father of Michigan school shooter found guilty of manslaughter weeks after mother’s conviction - CNN",
+          "description": "James Crumbley, the father of the teenager who killed four students at a Michigan high school in 2021, was found guilty of involuntary manslaughter.",
+          "url": "https://www.cnn.com/2024/03/14/us/james-crumbley-manslaughter-trial-thursday/index.html",
+          "urlToImage": "https://media.cnn.com/api/v1/images/stellar/prod/gettyimages-2074381090.jpg?c=16x9&q=w_800,c_fill",
+          "publishedAt": "2024-03-14T23:48:00Z",
+          "content": "James Crumbley, the father of the teenager who killed four students at a Michigan high school in 2021, was found guilty of involuntary manslaughter in a trial that comes a month after the shooters mo… [+6984 chars]"
+      },
+      {
+          "source": {
+              "id": null,
+              "name": "The Desert Sun"
+          },
+          "author": null,
+          "title": "Photos: Bees swarm stadium at BNP Paribas Open, halting match - Desert Sun",
+          "description": "A BNP Paribas Open match between Carlos Alcaraz and Alexander Zverev was halted Thursday when a swarm of bees descended on the Indian Wells stadium.",
+          "url": "https://www.desertsun.com/picture-gallery/sports/tennis/bnp/2024/03/14/bnp-paribas-open-bees-swarm-stadium-halting-match-photos/72979852007/",
+          "urlToImage": "https://www.desertsun.com/gcdn/authoring/authoring-images/2024/03/14/PPAS/72979314007-20240314-bnp-paribas-open-thursday-004.jpg?crop=2997,1686,x0,y249&width=2997&height=1686&format=pjpg&auto=webp",
+          "publishedAt": "2024-03-14T23:26:48Z",
+          "content": "A swarm of bees causes a delay as it covers the Spidercam during the Carlos Alcaraz vs. Alexander Zverev match at the BNP Paribas Open in Indian Wells Tennis Garden, CA, on March 14, 2024.\r\nBrandon M… [+2874 chars]"
+      },
+      {
+          "source": {
+              "id": null,
+              "name": "Daily Beast"
+          },
+          "author": "AJ McDougall",
+          "title": "New Details Emerge About Kate Middleton's Doctored Pic: Report - The Daily Beast",
+          "description": "The doctored image was retracted by multiple news agencies, with the AFP declaring Kensington Palace no longer a “trusted source.”",
+          "url": "https://www.thedailybeast.com/new-details-emerge-about-kate-middletons-photoshopped-pic-report-says",
+          "urlToImage": "https://img.thedailybeast.com/image/upload/c_crop,d_placeholder_euli9k,h_2813,w_5000,x_0,y_163/dpr_2.0/c_limit,w_740/f_auto,q_auto/v1710458765/GettyImages-1840560983_kzggps",
+          "publishedAt": "2024-03-14T23:26:43Z",
+          "content": "The rumors flying about the conspicuous absence of Catherine, Princess of Wales from the public eye since Christmas were only inflamed by the release of an evidently doctored image of her and her chi… [+861 chars]"
+      },
+      {
+          "source": {
+              "id": "associated-press",
+              "name": "Associated Press"
+          },
+          "author": "DASHA LITVINOVA",
+          "title": "Russians head to polls in a vote set to extend Putin's rule. His foes are in jail, in exile or dead - The Associated Press",
+          "description": "Voters are heading to the polls in Russia for a three-day presidential election that is all but certain to extend President Vladimir Putin’s rule by six more years after he stifled dissent. The election takes place against the backdrop of a ruthless crackdown…",
+          "url": "https://apnews.com/article/russia-election-2024-updates-227e65cb2a3f2c4cfdb58a9525fbe884",
+          "urlToImage": "https://dims.apnews.com/dims4/default/f4a7209/2147483647/strip/true/crop/6000x3375+0+311/resize/1440x810!/quality/90/?url=https%3A%2F%2Fassets.apnews.com%2F74%2Fd0%2F1e1c1ba29df00df8589840f18791%2F968a29df82694a8f9cf1c6d2c43e2101",
+          "publishedAt": "2024-03-14T23:25:00Z",
+          "content": "Voters headed to the polls in Russia on Friday for a three-day presidential election that is all but certain to extend President Vladimir Putins rule by six more years after he stifled dissent.\r\nThe … [+4761 chars]"
+      },
+      {
+          "source": {
+              "id": "espn",
+              "name": "ESPN"
+          },
+          "author": "ESPN",
+          "title": "Source: Jaguars to sign ex-49ers DL Armstead - ESPN",
+          "description": "Defensive lineman Arik Armstead is signing a 3-year, $51 million contract with the Jacksonville Jaguars, a source told ESPN's Adam Schefter.",
+          "url": "https://www.espn.com/nfl/story/_/id/39733201/jaguars-sign-ex-49ers-dl-arik-armstead-3-year-deal",
+          "urlToImage": "https://a3.espncdn.com/combiner/i?img=%2Fphoto%2F2024%2F0311%2Fr1303355_1296x729_16%2D9.jpg",
+          "publishedAt": "2024-03-14T22:18:00Z",
+          "content": "Mar 14, 2024, 05:18 PM ET\r\nDefensive lineman Arik Armstead is signing a 3-year, $51 million contract with the Jacksonville Jaguars, a source told ESPN's Adam Schefter.\r\nThe San Francisco 49ers releas… [+1685 chars]"
+      },
+      {
+          "source": {
+              "id": null,
+              "name": "[Removed]"
+          },
+          "author": null,
+          "title": "[Removed]",
+          "description": "[Removed]",
+          "url": "https://removed.com",
+          "urlToImage": null,
+          "publishedAt": "1970-01-01T00:00:00Z",
+          "content": "[Removed]"
+      },
+      {
+          "source": {
+              "id": null,
+              "name": "BBC News"
+          },
+          "author": null,
+          "title": "TikTok ban: China attacks 'bandit logic' of House vote - BBC.com",
+          "description": "After Congress takes a step towards banning the app over security concerns, TikTok says US jobs are at risk.",
+          "url": "https://www.bbc.com/news/world-us-canada-68564531",
+          "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/102B8/production/_132923266_gettyimages-2076693440.jpg",
+          "publishedAt": "2024-03-14T21:41:54Z",
+          "content": null
+      },
+      {
+          "source": {
+              "id": "cnn",
+              "name": "CNN"
+          },
+          "author": "By <a href=\"/profiles/jackie-wattles\">Jackie Wattles</a> and <a href=\"/profiles/adrienne-vogt\">Adrienne Vogt</a>, CNN",
+          "title": "SpaceX launches Starship rocket on third test flight - CNN",
+          "description": "The massive Starship rocket from Elon Musk's SpaceX launched for the third time Thursday. It achieved multiple milestones before likely breaking apart.",
+          "url": "https://www.cnn.com/world/live-news/spacex-starship-launch-scn-03-14-24/index.html",
+          "urlToImage": "https://cdn.cnn.com/cnnnext/dam/assets/240314095414-02-spacex-starship-launch-031424-super-tease.jpg",
+          "publishedAt": "2024-03-14T21:21:00Z",
+          "content": "Founder and CEO of SpaceX Elon Musk reacted to the latest Starship launch on Thursday, saying the world's most powerful rocket will \"make life multiplanetary.\"\r\nHe made the short comment with an acco… [+588 chars]"
+      },
+      {
+          "source": {
+              "id": null,
+              "name": "BBC News"
+          },
+          "author": null,
+          "title": "Vice-President Kamala Harris tours abortion clinic in historic first - BBC.com",
+          "description": "Her fellow Democrats are seeking to make abortion one of the defining issues of November's US elections.",
+          "url": "https://www.bbc.com/news/world-us-canada-68568383",
+          "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/17D92/production/_132928679_kamala.jpg",
+          "publishedAt": "2024-03-14T21:11:51Z",
+          "content": null
+      },
+      {
+          "source": {
+              "id": "the-verge",
+              "name": "The Verge"
+          },
+          "author": "Emilia David",
+          "title": "Google I/O 2024 is happening on May 14th - The Verge",
+          "description": "Google’s annual I/O developer conference will take place on May 14th. We expect to hear more about generative AI tools like Gemini as well as Chrome, Android, and Pixel.",
+          "url": "https://www.theverge.com/2024/3/14/24101171/google-io-2024-announced-may-14-android-ai-gemini",
+          "urlToImage": "https://cdn.vox-cdn.com/thumbor/EvlLUjBZF4-2CyW0Py-HizAp_iQ=/0x0:1850x1036/1200x628/filters:focal(925x518:926x519)/cdn.vox-cdn.com/uploads/chorus_asset/file/25336675/Screenshot_2024_03_14_at_4.16.44_PM.png",
+          "publishedAt": "2024-03-14T20:48:43Z",
+          "content": "Google I/O 2024 is happening on May 14th\r\nGoogle I/O 2024 is happening on May 14th\r\n / Expect more news about AI, Pixel, Android, and probably even more AI.\r\nByEmilia David, a reporter who covers AI.… [+1378 chars]"
+      },
+      {
+          "source": {
+              "id": "al-jazeera-english",
+              "name": "Al Jazeera English"
+          },
+          "author": "Al Jazeera",
+          "title": "Palestinian President Abbas appoints Mohammed Mustafa as prime minister - Al Jazeera English",
+          "description": "The move comes as the Palestinian Authority faces pressure to reform from the US amid Israel’s war on Gaza.",
+          "url": "https://www.aljazeera.com/news/2024/3/14/palestinian-authority-president-abbas-appoints-mohammed-mustafa-prime-minister",
+          "urlToImage": "https://www.aljazeera.com/wp-content/uploads/2024/03/AA-20240314-33990432-33990430-PALESTINIAN_PRESIDENT_ABBAS_APPOINTS_INVESTMENT_FUND_CHAIRMAN_MUSTAFA_AS_NEW_PRIME_MINISTER-1710444491.jpg?resize=1920%2C1440",
+          "publishedAt": "2024-03-14T20:43:17Z",
+          "content": "Palestinian President Mahmoud Abbas has appointed his longtime economic adviser Mohammed Mustafa to be the next prime minister in the face of US pressure to reform the Palestinian Authority as part o… [+3075 chars]"
+      },
+      {
+          "source": {
+              "id": "abc-news",
+              "name": "ABC News"
+          },
+          "author": "Kiara Alfonseca, Tesfaye Negussie",
+          "title": "LGBTQ teen Nex Benedict died by suicide, medical examiner says - ABC News",
+          "description": "Nex died one day after getting into a physical altercation with other students.",
+          "url": "https://abcnews.go.com/US/nex-benedict-died-suicide-medical-examiners-report-states/story?id=108093416",
+          "urlToImage": "https://i.abcnewsfe.com/a/6da9870b-7a15-451d-901e-926d94a1f2e2/nex-ht-er-240223_1708727882613_hpMain_3_16x9.jpg?w=1600",
+          "publishedAt": "2024-03-14T20:36:50Z",
+          "content": "Oklahoma 16-year-old Nex Benedict died by suicide, according to an autopsy report summary released Wednesday by the state's Office of the Chief Medical Examiner. The full report will be released on M… [+6563 chars]"
+      },
+      {
+          "source": {
+              "id": null,
+              "name": "Sports Illustrated"
+          },
+          "author": "Jonathan Harrison",
+          "title": "Adofo-Mensah tries to slam door on speculation that Vikings weren't aligned on Cousins - Sports Illustrated",
+          "description": "Were the Vikings not all aligned with Kirk Cousins?  Reading between the lines of Cousins' introductory interview with the Falcons it would certainly seem that way.",
+          "url": "https://www.si.com/fannation/bringmethesports/vikings/adofo-mensah-tries-to-slam-door-on-speculation-that-vikings-werent-aligned-on-cousins",
+          "urlToImage": "https://www.si.com/.image/ar_1.91%2Cc_fill%2Ccs_srgb%2Cg_faces:center%2Cq_auto:good%2Cw_1200/MjA1MDIwMzMyODYyNDgyMTcy/screen-shot-2024-03-14-at-23724-pm.png",
+          "publishedAt": "2024-03-14T20:34:58Z",
+          "content": "Reading between the lines of Kirk Cousins' first day with the Atlanta Falcons, it certainly seems like the Vikings and Cousins had very different views on the best way forward.\r\n\"In Minnesota, over t… [+3487 chars]"
+      },
+      {
+          "source": {
+              "id": null,
+              "name": "FOX 9"
+          },
+          "author": "FOX 9",
+          "title": "Uber and Lyft in Minneapolis: Ride-hailing apps say May 1 will be final day - FOX 9 Minneapolis-St. Paul",
+          "description": "Uber and Lyft have now set their deadline to stop service in Minneapolis after a city council vote on Thursday.",
+          "url": "https://www.fox9.com/news/uber-and-lyft-to-leave-minneapolis",
+          "urlToImage": "https://images.foxtv.com/static.fox9.com/www.fox9.com/content/uploads/2024/02/1280/720/lyft-uber-logos.jpg?ve=1&tl=1",
+          "publishedAt": "2024-03-14T20:09:31Z",
+          "content": "MINNEAPOLIS (FOX 9) - After the Minneapolis City Council voted 10-3 to override Mayor Jacob Frey's veto of a ride-hailing pay raise ordinance on Thursday, Uber says it will pull out of the entire Twi… [+1746 chars]"
+      },
+      {
+          "source": {
+              "id": "cnn",
+              "name": "CNN"
+          },
+          "author": "Jen Christensen, Amanda Musa",
+          "title": "All eligible people at Chicago migrant shelter have been vaccinated for measles in ‘unprecedented operation’ - CNN",
+          "description": "The Chicago Department of Public Health said Wednesday that everyone who is eligible for vaccination at a temporary shelter housing migrants that’s at the center of a measles outbreak has now been vaccinated.",
+          "url": "https://www.cnn.com/2024/03/13/health/chicago-migrant-shelter-measles-vaccinated-in-unprecedented-operation/index.html",
+          "urlToImage": "https://media.cnn.com/api/v1/images/stellar/prod/ap24073708103317.jpg?c=16x9&q=w_800,c_fill",
+          "publishedAt": "2024-03-14T20:04:00Z",
+          "content": "The Chicago Department of Public Health said Wednesday that everyone who is eligible for vaccination at a temporary shelter housing migrants thats at the center of a measles outbreak has now been vac… [+7291 chars]"
+      },
+      {
+          "source": {
+              "id": "bloomberg",
+              "name": "Bloomberg"
+          },
+          "author": "Joe Deaux",
+          "title": "US Steel Deal Latest: Cleveland Weighs Low Bid With Union Support - Bloomberg",
+          "description": "Cleveland-Cliffs Inc. Chief Executive Officer Lourenco Goncalves said he’d consider another bid — with union support — for United States Steel Corp., albeit at a significantly lower price than the existing offer from Nippon Steel Corp.",
+          "url": "https://www.bloomberg.com/news/articles/2024-03-14/cliffs-ceo-weighs-lowball-bid-for-us-steel-with-union-backing",
+          "urlToImage": "https://assets.bwbx.io/images/users/iqjWHBFdfxIU/iUqxEKy.lpbk/v1/1200x799.jpg",
+          "publishedAt": "2024-03-14T19:27:50Z",
+          "content": "Cleveland-Cliffs Inc. Chief Executive Officer Lourenco Goncalves said hed consider another bid with union support for United States Steel Corp., albeit at a significantly lower price than the existin… [+316 chars]"
+      },
+      {
+          "source": {
+              "id": null,
+              "name": "Yahoo Entertainment"
+          },
+          "author": "Ines Ferré",
+          "title": "Stock market today: Stocks slump after hot inflation print - Yahoo Finance",
+          "description": "Stocks edge higher despite a hotter-than-expected wholesale inflation print and rebounding retail sales, data that could sway the Federal Reserve at its...",
+          "url": "https://finance.yahoo.com/news/live/stock-market-today-stocks-slump-after-hot-inflation-print-160639297.html",
+          "urlToImage": "https://s.yimg.com/ny/api/res/1.2/.qkXcRkoWbRfDs0yYG84ew--/YXBwaWQ9aGlnaGxhbmRlcjt3PTEyMDA7aD04MDA-/https://s.yimg.com/os/creatr-uploaded-images/2024-03/2a3ed9c0-deab-11ee-9ffd-ef1a1f7ff3ea",
+          "publishedAt": "2024-03-14T19:15:00Z",
+          "content": "Lennars (LEN) CEO Stuart Miller said Thursday that affordability remains a headwind for would-be buyers as mortgage rates linger around the 7% mark.\r\nMiller said on the company's first quarter earnin… [+1676 chars]"
+      },
+      {
+          "source": {
+              "id": null,
+              "name": "BBC News"
+          },
+          "author": null,
+          "title": "Michael Gove names groups as he unveils extremism definition - BBC.com",
+          "description": "The communities secretary has told MPs of five groups to be assessed against the new definition.",
+          "url": "https://www.bbc.com/news/uk-politics-68564577",
+          "urlToImage": "https://ichef.bbci.co.uk/news/1024/branded_news/503E/production/_132924502_govecommonsukparliament.png",
+          "publishedAt": "2024-03-14T19:10:03Z",
+          "content": null
+      },
+      {
+          "source": {
+              "id": null,
+              "name": "Quartz India"
+          },
+          "author": "Melvin Backman",
+          "title": "Ukraine is bombing Russian oil refineries right when Russia really needs its oil money - Quartz",
+          "description": "Military spending is one of the only things helping Russia's economy grow",
+          "url": "https://qz.com/ukraine-drone-strike-russia-oil-elections-economy-1851336275",
+          "urlToImage": "https://i.kinja-img.com/image/upload/c_fill,h_675,pg_1,q_80,w_1200/e1e35fb89a3e78e54e3895fa0d318626.jpg",
+          "publishedAt": "2024-03-14T19:05:00Z",
+          "content": "Its been a bit of whiplash this week the world of Russian oil. \r\nOn Tuesday, Bloomberg reported that the countrys oil exports had crept up to 3.7 million barrels a day, their highest level this year.… [+1563 chars]"
+      }
+  ]
+}
+
+export default mockData;
